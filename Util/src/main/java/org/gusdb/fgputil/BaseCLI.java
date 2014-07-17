2a9f995fff61482dfc5748d2f70d72f60a364a78
Ryan Doherty <tech@conical.org>
2014-07-17 16:22:26 +0000

    Rename private vars and make internal class static for clarity
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/FgpUtil/trunk@14293 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Util/src/main/java/org/gusdb/fgputil/BaseCLI.java
+++ Util/src/main/java/org/gusdb/fgputil/BaseCLI.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package org.gusdb.fgputil;
 
 import java.util.HashSet;
@@ -25,20 +22,18 @@ public abstract class BaseCLI {
 
   protected static final String ARG_PROJECT_ID = "model";
 
-  private String command;
-  private String description;
-  protected String footer;
+  private String _command;
+  private String _description;
 
-  private Options options;
-  private Map<String, Object> defaults;
-  private CommandLine commandLine;
+  private Options _options;
+  private Map<String, Object> _defaults;
+  private CommandLine _commandLine;
 
   protected BaseCLI(String command, String description) {
-    this.command = (command == null) ? getClass().getSimpleName() : command;
-    this.description = description;
-    this.footer = "";
-    options = new Options();
-    defaults = new LinkedHashMap<String, Object>();
+    _command = (command == null) ? getClass().getSimpleName() : command;
+    _description = description;
+    _options = new Options();
+    _defaults = new LinkedHashMap<String, Object>();
 
     declareOptions();
   }
@@ -63,22 +58,22 @@ public abstract class BaseCLI {
 
   private void parseCommandLine(String[] args) throws ParseException {
     CommandLineParser parser = new BasicParser();
-    commandLine = parser.parse(options, args);
+    _commandLine = parser.parse(_options, args);
   }
 
   public void printUsage() {
     String newline = System.getProperty("line.separator");
 
-    StringBuffer syntax = new StringBuffer(command);
+    StringBuffer syntax = new StringBuffer(_command);
 
     // group options by groups
     Set<OptionGroup> requiredGroups = new HashSet<OptionGroup>();
     Set<OptionGroup> optionalGroups = new HashSet<OptionGroup>();
     Set<Option> requiredOptions = new HashSet<Option>();
     Set<Option> optionalOptions = new HashSet<Option>();
-    for (Object obj : options.getOptions()) {
+    for (Object obj : _options.getOptions()) {
       Option option = (Option) obj;
-      OptionGroup group = options.getOptionGroup(option);
+      OptionGroup group = _options.getOptionGroup(option);
       if (group == null) {
         if (option.isRequired())
           requiredOptions.add(option);
@@ -114,28 +109,28 @@ public abstract class BaseCLI {
         syntax.append("]");
     }
 
-    String header = newline + description + newline + newline + "Options:";
+    String header = newline + _description + newline + newline + "Options:";
 
     String footer = " ";
 
     // PrintWriter stderr = new PrintWriter(System.err);
     HelpFormatter formatter = new HelpFormatter();
-    formatter.printHelp(syntax.toString(), header, options, footer);
+    formatter.printHelp(syntax.toString(), header, _options, footer);
   }
 
   protected void addNonValueOption(String name, Boolean defaultValue, String description) {
     Option option = new Option(name, name, false, description);
-    options.addOption(option);
-    defaults.put(name, defaultValue);
+    _options.addOption(option);
+    _defaults.put(name, defaultValue);
   }
 
   protected void addSingleValueOption(String name, boolean required, String defaultValue, String description) {
     Option option = new Option(name, name, true, description);
     option.setRequired(required);
     option.setArgs(1);
-    options.addOption(option);
+    _options.addOption(option);
 
-    defaults.put(name, defaultValue);
+    _defaults.put(name, defaultValue);
   }
 
   protected void addMultiValueOption(String name, boolean required, int numArgs, String[] defaultValues,
@@ -146,21 +141,21 @@ public abstract class BaseCLI {
     Option option = new Option(name, name, true, description);
     option.setRequired(required);
     option.setArgs(numArgs);
-    options.addOption(option);
+    _options.addOption(option);
 
-    defaults.put(name, defaultValues);
+    _defaults.put(name, defaultValues);
   }
 
   protected void addGroup(boolean groupRequired, String... optionNames) {
     OptionGroup group = new OptionGroup();
     group.setRequired(groupRequired);
     for (String optionName : optionNames) {
-      Option option = options.getOption("-" + optionName);
+      Option option = _options.getOption("-" + optionName);
       if (option == null)
         throw new NullPointerException("Option '" + optionName + "' does not exist.");
       group.addOption(option);
     }
-    options.addOptionGroup(group);
+    _options.addOptionGroup(group);
   }
 
   /**
@@ -171,16 +166,16 @@ public abstract class BaseCLI {
    *         be returned.
    */
   protected Object getOptionValue(String name) {
-    if (!commandLine.hasOption(name))
-      return defaults.get(name);
+    if (!_commandLine.hasOption(name))
+      return _defaults.get(name);
 
-    Option option = options.getOption("-" + name);
+    Option option = _options.getOption("-" + name);
     if (!option.hasArg())
       return true;
     else if (option.getArgs() == 1)
-      return commandLine.getOptionValue(name);
+      return _commandLine.getOptionValue(name);
     else
-      return commandLine.getOptionValues(name);
+      return _commandLine.getOptionValues(name);
   }
 
   private void printOptionGroups(StringBuffer syntax, Set<OptionGroup> groups) {
