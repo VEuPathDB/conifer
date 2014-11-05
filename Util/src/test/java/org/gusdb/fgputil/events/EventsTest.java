df2fd901182e93724d94354f8683dbddce23e30c
Ryan Doherty <tech@conical.org>
2014-11-05 19:54:14 +0000

    Add comments and check for total events received
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/FgpUtil/trunk@14956 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Util/src/test/java/org/gusdb/fgputil/events/EventsTest.java
+++ Util/src/test/java/org/gusdb/fgputil/events/EventsTest.java
@@ -1,6 +1,7 @@
 package org.gusdb.fgputil.events;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.gusdb.fgputil.FormatUtil;
 import org.gusdb.fgputil.events.ListenerStatus.Status;
@@ -9,6 +10,8 @@ import org.junit.Test;
 public class EventsTest implements EventListener {
 
   private static final String THROW_ERROR = "throw_error";
+
+  private static int totalEventsReceived = 0;
 
   public static class TestEvent extends Event {
 
@@ -29,17 +32,52 @@ public class EventsTest implements EventListener {
   @Test
   public void testEvents() throws Exception {
     try {
+      // initialize events framework with default configuration
       Events.init();
+
+      // subscribe to events
       Events.subscribe(this, TestEvent.TEST_EVENT_CODE);
       Events.subscribe(this, ListenerExceptionEvent.class);
       Events.subscribe(new EventListener() {
         @Override public void eventTriggered(Event event) throws Exception {
+          totalEventsReceived++;
           System.out.println("Event submitted with code: " + event.getEventCode());
         }}, Event.class);
+
+      // trigger an event where we expect all listeners to succeed
       Status status = getStatusWhenSubmitting("some message");
       assertEquals(status, Status.SUCCESS);
+
+      // trigger an event where we expect a listener to fail
       status = getStatusWhenSubmitting(THROW_ERROR);
       assertEquals(status, Status.ERRORED);
+
+      // trigger an event were we expect all listeners to succed using triggerAndWait()
+      try {
+        Events.triggerAndWait(new TestEvent("some message"), new Exception("failed!"));
+        assertTrue(true);
+      }
+      catch(Exception e) {
+        assertTrue(false);
+      }
+
+      // trigger an event were we expect all listeners to succed using triggerAndWait()
+      try {
+        Events.triggerAndWait(new TestEvent(THROW_ERROR), new Exception("failed!"));
+        assertTrue(false);
+      }
+      catch(Exception e) {
+        assertTrue(true);
+      }
+
+      // wait for error events to be received; since error events are simply
+      //   triggered (not waited for), the number-received check below sometimes
+      //   fails without yielding to the error event processing threads
+      // NOTE: this is still a race condition and may fail occasionally!!
+      sleep(1000);
+
+      // make sure the correct number of events have been processed
+      assertEquals(12, totalEventsReceived);
     }
     finally {
       Events.shutDown();
@@ -59,6 +97,7 @@ public class EventsTest implements EventListener {
 
   @Override
   public void eventTriggered(Event event) throws Exception {
+    totalEventsReceived++;
     System.out.println("Received event with code '" + event.getEventCode() + "' of type: " + event.getClass().getName());
     if (event instanceof TestEvent) {
       sleep(100);
