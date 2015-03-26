7720693b2be81bd89bcfb44f27a1f4e9a4dd6101
Ryan Doherty <tech@conical.org>
2015-03-26 18:27:58 +0000

    #19012: combine and refactor SVN branch and checkout scripts for consistent project management
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/FgpUtil/trunk@15794 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Util/lib/perl/ProjectBrancher.pm
+++ Util/lib/perl/ProjectBrancher.pm
@@ -1,68 +1,93 @@
+###############################################################################
+##
+##  This package provides utilities to interact with the EuPathDB
+##  Subversion repository.
+##
+###############################################################################
+
 package FgpUtil::Util::ProjectBrancher;
 
+use base 'Exporter';
 use strict;
+use warnings;
 
-sub branch {
-    my ($branch, $comment, $delete, $projects) = @_;
+our @EXPORT = qw(getRepoBaseUrl getCmd getProjectGroup getProjectGroupOptions printProjectGroupInfo);
 
-    usage() unless ($branch && $comment);
+# Base URL for EuPathDB Subversion repositories
+my $url = 'https://www.cbil.upenn.edu/svn';
 
-    if ($delete) {
-	print "\nYou are about to DELETE the $branch branch\nType the name of the branch to confirm: ";
-	my $confirm = <STDIN>;
-	chomp $confirm;
-	die "You did not correctly confirm the branch\n" unless $branch eq $confirm;
-    }
+# Base projects
+my @baseProjects = ('install:gus', 'CBIL:gus', 'FgpUtil:gus', 'WSF:gus', 'WDK:gus');
 
-    my $url = 'https://www.cbil.upenn.edu/svn';
-    foreach my $project (@$projects) {
-	my ($name, $repository) = split(/:/, $project);
-	my $target = "$url/$repository/$name/branches/$branch";
-	`svn ls $target &> /dev/null`;
-	my $err = $? >>8;
-	if ($delete) {
-	    if ($err != 0) {
-		print STDERR "$name does not exist. skipping\n";
-		next;
-	    }
-	    my $cmd = "svn delete -m \"deleting branch $name: $comment\" $target";
-	    print STDERR "deleting $name\n";
-	    system($cmd) == 0 || die "Failed deleting '$name'\n";
-	} else {
-	    if ($err == 0) {
-		print STDERR "$name already branched. skipping\n";
-		next;
-	    }
-	    my $cmd = "svn cp -m \"creating branch $name: $comment\" $url/$repository/$name/trunk $target";
-	    print STDERR "branching $name\n";
-	    system($cmd) == 0 || die "Failed branching '$name'\n";
-	}
-    }
+# Backend/data projects
+my @backendProjects = ('GusAppFramework:gus', 'GusSchema:gus', 'ReFlow:gus',
+                       'DoTS:apidb', 'GGTools:apidb',
+                       'ApiCommonData:apidb', 'ApiCommonWorkflow:apidb',
+                       'ApiCommonShared:apidb', 'EuPathDatasets:apidb',
+                       'OrthoMCLData:apidb', 'OrthoMCLWorkflow:apidb', 
+                       'OrthoMCLShared:apidb', 'OrthoMCLEngine:apidb');
+
+# Shared website projects
+my @sharedSiteProjects = ('EuPathSiteCommon:apidb', 'EuPathWebSvcCommon:apidb');
+
+# Additional projects for Api sites
+my @apiSiteProjects = ('EuPathPresenters:apidb', 'EuPathDatasets:apidb',
+                       'ReFlow:gus', 'ApiCommonShared:apidb','GBrowse:apidb',
+                       'ApiCommonWebsite:apidb', 'ApiCommonWebService:apidb');
+
+# Additional projects for Ortho
+my @orthoSiteProjects = ('OrthoMCLShared:apidb', 'OrthoMCLWebsite:apidb',
+                         'OrthoMCLWebService:apidb');
+
+# All other projects not listed above
+my @otherProjects = ('DJob:gus', 'TuningManager:gus', 'OAuth2Server:gus',
+                     'WDKTemplateSite:gus', 'WSFTemplate:gus');
+
+# Keys for selecting project groups
+sub uniq { my %seen; grep !$seen{$_}++, @_; }
+my %groupsMap = (
+    'backend' => [uniq((@baseProjects, @backendProjects))],
+    'apisite' => [uniq((@baseProjects, @sharedSiteProjects, @apiSiteProjects))],
+    'orthosite' => [uniq((@baseProjects, @sharedSiteProjects, @orthoSiteProjects))],
+    'allsite' => [uniq((@baseProjects, @sharedSiteProjects, @apiSiteProjects, @orthoSiteProjects))],
+    'all' => [uniq((@baseProjects, @backendProjects, @sharedSiteProjects, @apiSiteProjects, @orthoSiteProjects, @otherProjects))]
+);
+
+sub getRepoBaseUrl {
+    return $url;
 }
 
-sub usage {
-  my ($name, $projects) = @_;
+sub getCmd {
+    # get name of command that was run
+    $0 =~ /(\w+)$/;
+    return $1;
+}
 
-  $0 =~ /(\w+)$/;   # name of command that was run
+sub getProjectGroup {
+    my $groupKey = $_[0];
+    if (exists $groupsMap{$groupKey}) {
+        my $arrayRef = $groupsMap{$groupKey};
+        return @$arrayRef;
+    }
+    # bad project key; set error and return undefined
+    $! = "Bad project group '$groupKey'; valid values are: " . join(', ', keys(%groupsMap));
+    return undef;
+}
 
-  my $prog = $1;
+sub getProjectGroupOptions {
+    return '(' . join('|', keys(%groupsMap)) . ')';
+}
 
-  print STDERR "
-Branch all of the $name software using a single branch name. Operates directly in svn, not your local dirs.
-
-usage: $prog branch_name comment [-delete]
-
--delete deletes the branch from all projects.
-
-Branches these projects:
-";
-
- foreach my $project (@$projects) {
-    my ($name, $repository) = split(/:/, $project);
-    print STDERR "  $name\n";
-  }
-
-exit(1);
+sub printProjectGroupInfo {
+    foreach my $key (keys %groupsMap) {
+        print STDERR "    $key: ";
+        my $arrayRef = $groupsMap{$key};
+        foreach my $project (@$arrayRef) {
+            my ($name, $repository) = split(/:/, $project);
+            print STDERR "$name, ";
+        }
+        print STDERR "\n\n";
+    }
 }
 
 1;
