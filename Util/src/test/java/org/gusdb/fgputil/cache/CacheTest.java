e876443a62044759d7a205cfb8aecd6c1e36cb92
Ryan Doherty <tech@conical.org>
2015-08-30 06:14:11 +0000

    Handle fetch problems better, add getSize() method, improve unit tests for ItemCache
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/FgpUtil/trunk@16598 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Util/src/test/java/org/gusdb/fgputil/cache/CacheTest.java
+++ Util/src/test/java/org/gusdb/fgputil/cache/CacheTest.java
@@ -1,17 +1,34 @@
 package org.gusdb.fgputil.cache;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.apache.log4j.Logger;
+import org.gusdb.fgputil.FormatUtil;
 import org.junit.Test;
 
 public class CacheTest {
 
   private static final Logger LOG = Logger.getLogger(CacheTest.class);
-  
+  private static final boolean LOG_ON = false;
+
   private static class StringFetcher implements ItemFetcher<Integer,String> {
+
+    private final List<String> _opOrder;
+
+    public StringFetcher(List<String> opOrder) {
+      _opOrder = opOrder;
+    }
 
     @Override
     public String fetchItem(Integer itemId) throws UnfetchableItemException {
-      LOG.info("Fetching item " + itemId);
+      if (LOG_ON) LOG.info("Fetching item " + itemId);
+      _opOrder.add("b" + itemId);
       return String.valueOf(itemId);
     }
 
@@ -25,19 +42,73 @@ public class CacheTest {
       return false;
     }
   }
-  
-  @Test
-  public void testCache() throws Exception {
-    int[] idsToLookUp = { 1, 3, 5, 3, 4, 1, 5, 2, 3, 1, 7, 6, 3 };
-    StringFetcher fetcher = new StringFetcher();
-    ItemCache<Integer,String> cache = new ItemCache<>(5, 3);
-    for (int id : idsToLookUp) {
-      LOG.info("Asking for item " + id);
-      cache.getItem(id, fetcher);
-      if (id == 4) {
-        cache.expireCachedItems(3);
+
+  private class Worker implements Runnable {
+
+    private final ItemCache<Integer, String> _cache;
+    private final List<String> _opOrder;
+    private final AtomicInteger _completedCount;
+
+    public Worker(ItemCache<Integer, String> cache, List<String> opOrder, AtomicInteger completedCount) {
+      _cache = cache;
+      _opOrder = opOrder;
+      _completedCount = completedCount;
+    }
+
+    @Override
+    public void run() {
+      int[] idsToLookUp = { 1, 3, 5, 3, 4, 1, 5, 2, 3, 1, 7, 6, 3 };
+      StringFetcher fetcher = new StringFetcher(_opOrder);
+      for (int id : idsToLookUp) {
+        if (LOG_ON) LOG.info("Asking for item " + id);
+        _opOrder.add("a" + id);
+        try {
+          _cache.getItem(id, fetcher);
+        }
+        catch (UnfetchableItemException e) {
+          // should never happen
+          LOG.error("Error fetching", e);
+        }
+        int size = _cache.getSize();
+        if (LOG_ON) LOG.info("Current size: " + size);
+        _opOrder.add("s" + size);
+        if (id == 4) {
+          _cache.expireCachedItems(3);
+          size = _cache.getSize();
+          if (LOG_ON) LOG.info("Current size: " + size);
+          _opOrder.add("s" + size);
+        }
       }
+      _completedCount.incrementAndGet();
     }
   }
+
+  @Test
+  public void singleThreadTest() throws Exception {
+    LOG.info("Starting single thread test");
+    ItemCache<Integer,String> cache = new ItemCache<>(5, 3);
+    List<String> opOrder = new Vector<>();
+    AtomicInteger completedCount = new AtomicInteger(0);
+    Executors.newSingleThreadExecutor().execute(new Worker(cache, opOrder, completedCount));
+    while (completedCount.get() < 1) {}
+    String expected = "[ a1,b1,s1,a3,b3,s2,a5,b5,s3,a3,s3,a4,b4,s4,s3,a1,s3,a5,s3,a2,b2,s4,a3,b3,s5,a1,s5,a7,b7,s3,a6,b6,s4,a3,s4 ]";
+    String actual = FormatUtil.arrayToString(opOrder.toArray(), ",");
+    LOG.info("Result: " + actual);
+    assertEquals(expected, actual);
+  }
   
+  @Test
+  public void multiThreadTest() throws Exception {
+    LOG.info("Starting multi-thread test");
+    int numThreads = 100;
+    ItemCache<Integer,String> cache = new ItemCache<>(4, 2);
+    List<String> opOrder = new Vector<>();
+    AtomicInteger completedCount = new AtomicInteger(0);
+    Executor exec = Executors.newFixedThreadPool(10);
+    for (int i = 0; i < numThreads; i++) {
+      exec.execute(new Worker(cache, opOrder, completedCount));
+    }
+    while (completedCount.get() < numThreads) {}
+    LOG.info("Number of ops: " + opOrder.size());
+  }
 }
