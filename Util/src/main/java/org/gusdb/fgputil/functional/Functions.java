96d5f4e83c97f8f3a2c13adda5d380a8ff7167dd
Cris Lawrence <crisl@upenn.edu>
2016-03-22 20:22:47 +0000

    Added two functional style filter methods.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/FgpUtil/branches/gus4@17997 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Util/src/main/java/org/gusdb/fgputil/functional/Functions.java
+++ Util/src/main/java/org/gusdb/fgputil/functional/Functions.java
@@ -1,9 +1,11 @@
 package org.gusdb.fgputil.functional;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.gusdb.fgputil.functional.FunctionalInterfaces.Function;
+import org.gusdb.fgputil.functional.FunctionalInterfaces.Predicate;
 
 public class Functions {
 
@@ -14,4 +16,37 @@ public class Functions {
     }
     return result;
   }
+  
+  /**
+   * Using the iterable collection and the predicate function passed in, filters those
+   * items that satisfy the predicate into a new list
+   * @param inputs - iterable collection to filter
+   * @param predicate - predicate function supplying the filter test
+   * @return - new list of filtered items
+   */
+  public static <T> List<T> filter(Iterable<T> inputs, Predicate<T> predicate) {
+    List<T> result = new ArrayList<>();
+    for(T obj : inputs) {
+      if(predicate.test(obj)) {
+        result.add(obj);
+      }  
+    }
+    return result;
+  }
+  
+  /**
+   * Using the iterable collection and the predicate function passed in, removes from the
+   * given collection, those items that do not satisfy the predicate.
+   * @param inputs - iterable collection to be modified
+   * @param predicate - predicate function supplying the filter test
+   */
+  public static <T> void filterInPlace(Iterable<T> inputs, Predicate<T> predicate) {
+    Iterator<T> iterator = inputs.iterator();
+    while(iterator.hasNext()) {
+      if(!predicate.test(iterator.next())) {
+        iterator.remove();
+      }
+    }
+  }
+
 }
