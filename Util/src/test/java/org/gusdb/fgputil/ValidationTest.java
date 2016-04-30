5ff6cc22c9ab674cafb191e55816d4c52b0008ec
Ryan Doherty <tech@conical.org>
2016-04-30 06:02:07 +0000

    Convert to real unit test
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/FgpUtil/branches/gus4@18376 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Util/src/test/java/org/gusdb/fgputil/ValidationTest.java
+++ Util/src/test/java/org/gusdb/fgputil/ValidationTest.java
@@ -1,6 +1,6 @@
 package org.gusdb.fgputil;
 
-import static java.util.Collections.EMPTY_SET;
+import static org.junit.Assert.assertEquals;
 import static java.util.Arrays.asList;
 
 import java.util.Arrays;
@@ -8,6 +8,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.List;
 
+import org.json.JSONObject;
 import org.junit.Test;
 
 /**
@@ -16,9 +17,60 @@ import org.junit.Test;
  */
 public class ValidationTest {
 
-  private static final String[] someProps = { "a", "c", "d", "e" };
   private static final String[] requiredProps = { "a", "b", "c" };
   private static final String[] optionalProps = { "d" };
+
+  private static final String[] testSet1 = { "a", "b", "c" };
+  private static final String[] testSet2 = { "a", "b", "c", "d" };
+  private static final String[] testSet3 = { "a", "c", "d", "e" };
+
+  private static final String[] NONE = { };
+
+  @Test
+  public void doTestSet1() {
+    runTests(1, testSet1, new Integer[]{ 0, 3, 0, 3, 0, 0, 0, 0 });
+  }
+
+  @Test
+  public void doTestSet2() {
+    runTests(2, testSet2, new Integer[]{ 0, 3, 1, 4, 0, 0, 0, 0 });
+  }
+
+  @Test
+  public void doTestSet3() {
+    runTests(3, testSet3, new Integer[]{ 2, 3, 3, 4, 1, 0, 1, 0 });
+  }
+
+  private void runTests(int testSetNum, String[] testSet, Integer[] expectedResults) {
+    doTest(testSetNum + "a", testSet, requiredProps, optionalProps, false, expectedResults[0]);
+    doTest(testSetNum + "b", testSet, NONE, optionalProps, false, expectedResults[1]);
+    doTest(testSetNum + "c", testSet, requiredProps, NONE, false, expectedResults[2]);
+    doTest(testSetNum + "d", testSet, NONE, NONE, false, expectedResults[3]);
+    doTest(testSetNum + "e", testSet, requiredProps, optionalProps, true, expectedResults[4]);
+    doTest(testSetNum + "f", testSet, NONE, optionalProps, true, expectedResults[5]);
+    doTest(testSetNum + "g", testSet, requiredProps, NONE, true, expectedResults[6]);
+    doTest(testSetNum + "h", testSet, NONE, NONE, true, expectedResults[7]);
+  }
+
+  private static void doTest(String testNumber, String[] properties, String[] requiredProps,
+      String[] optionalProps, boolean allowExtra, int numExpectedMessages) {
+    log("Test " + testNumber + ":");
+    List<String> messages = ValidationUtil.validateProperties(
+        asList(properties), toSet(requiredProps), toSet(optionalProps), allowExtra);
+    log(messages);
+    assertEquals(numExpectedMessages, messages.size());
+  }
+
+  // just checking for runtime errors when converting JSON
+  @Test
+  public void testJson() {
+    JSONObject json = new JSONObject();
+    for (String s : testSet3) {
+      json.put(s, true);
+    }
+    List<String> messages = ValidationUtil.validateProperties(json, toSet(requiredProps), toSet(optionalProps));
+    assertEquals(2, messages.size());
+  }
 
   private static Set<String> toSet(String[] arr) {
     return new HashSet<>(Arrays.asList(arr));
@@ -33,13 +85,4 @@ public class ValidationTest {
       System.out.println(message);
     }
   }
-
-  @Test
-  public void testValidationUtil() {
-    log("Test 1:");
-    log(ValidationUtil.validateProperties(asList(someProps), toSet(requiredProps), toSet(optionalProps)));
-    log("Test 2:");
-    log(ValidationUtil.validateProperties(asList(someProps), toSet(requiredProps), EMPTY_SET));
-  }
-
 }
