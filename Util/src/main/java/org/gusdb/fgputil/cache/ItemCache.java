c10cee0602de2d402ebe79ff38f46de9d9a5a94b
Ryan Doherty <tech@conical.org>
2016-08-24 16:40:47 +0000

    Improve generics param documentation
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/FgpUtil/trunk@18939 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Util/src/main/java/org/gusdb/fgputil/cache/ItemCache.java
+++ Util/src/main/java/org/gusdb/fgputil/cache/ItemCache.java
@@ -14,8 +14,8 @@ import org.apache.log4j.Logger;
  * thread-safe and can be used in a static, application-wide way to cache
  * homogeneous objects.
  * 
- * @param S unique keys of items cached
- * @param T item cached
+ * @param S type of unique keys of items cached
+ * @param T type of items cached
  * 
  * @author rdoherty
  */
