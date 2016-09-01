35fbc532c8067696a3d44af16a398b1fa5edad88
Ryan Doherty <tech@conical.org>
2016-09-01 07:27:53 +0000

    Add simple timer class
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/FgpUtil/trunk@19018 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Util/src/main/java/org/gusdb/fgputil/Timer.java
@@ -0,0 +1,55 @@
+package org.gusdb.fgputil;
+
+/**
+ * Provides simple timer class for measuring duration of clock time between events.  Also provides
+ * human-readable toString of time elapsed.
+ * 
+ * @author rdoherty
+ */
+public class Timer {
+
+  private long _startTime;
+
+  public static Timer start() {
+    return new Timer();
+  }
+
+  public Timer() {
+    restart();
+  }
+
+  public void restart() {
+    _startTime = System.currentTimeMillis();
+  }
+
+  public long getElapsed() {
+    return System.currentTimeMillis() - _startTime;
+  }
+
+  public String getElapsedAsString() {
+    return getDurationAsString(getElapsed());
+  }
+
+  public static String getDurationAsString(long totalMillis) {
+    long millis = totalMillis % 1000;
+    long totalSeconds = totalMillis / 1000;
+    if (totalSeconds == 0) return  millis + "ms";
+    long seconds = totalSeconds % 60;
+    long totalMinutes = totalSeconds / 60;
+    if (totalMinutes == 0) return seconds + "." + millis + " seconds";
+    long minutes = totalMinutes % 60;
+    long hours = totalMinutes / 60;
+    if (hours == 0) return minutes + ":" + pad10(seconds) + "." + millis;
+    return hours + ":" + pad10(minutes) + ":" + pad10(seconds) + "." + millis;
+  }
+
+  private static String pad10(long minutes) {
+    if (minutes < 10) return "0" + minutes;
+    return String.valueOf(minutes);
+  }
+
+  @Override
+  public String toString() {
+    return getElapsedAsString();
+  }
+}
