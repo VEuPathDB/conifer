08d2977fe5d45a9c122a501964099baa6b8319ac
Ryan Doherty <tech@conical.org>
2016-09-07 04:18:06 +0000

    Add unit tests for new JSON helper classes
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/FgpUtil/trunk@19036 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Util/src/test/java/org/gusdb/fgputil/JsonIteratorsTest.java
@@ -0,0 +1,61 @@
+package org.gusdb.fgputil;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.gusdb.fgputil.JsonIterators.JsonArrayIterable;
+import org.gusdb.fgputil.JsonIterators.JsonObjectIterable;
+import org.junit.Test;
+
+public class JsonIteratorsTest {
+
+  @Test
+  public void testArrayIter() throws Exception {
+    JSONArray array = new JSONArray("[]");
+    testCount(array, 0);
+    array = new JSONArray("[ 2, null, true, 'blah' ]");
+    testCount(array, 4);
+  }
+
+  private void testCount(JSONArray array, int expected) {
+    int count = 0;
+    List<String> vals = new ArrayList<>();
+    for (JsonType value : new JsonArrayIterable(array)) {
+      count++;
+      vals.add(value.toString());
+    }
+    System.out.println(FormatUtil.arrayToString(vals.toArray()));
+    assertEquals(expected, count);
+  }
+
+  @Test
+  public void testObjectIter() throws Exception {
+    JSONObject obj = new JSONObject("{}");
+    testCount(obj, 0);
+    obj = new JSONObject("{ 1: b, blah: null, a: true, 4: 6.78E34, true: false, null: [], []: {}, {}: [] }");
+    testCount(obj, 8);
+  }
+
+  private void testCount(JSONObject obj, int expected) {
+    int count = 0;
+    List<String> keys = new ArrayList<>();
+    List<String> vals = new ArrayList<>();
+    List<String> types = new ArrayList<>();
+    for (Entry<String, JsonType> entry : new JsonObjectIterable(obj)) {
+      count++;
+      keys.add(entry.getKey());
+      vals.add(entry.getValue().toString());
+      types.add(entry.getValue().getType().name());
+    }
+    System.out.println(FormatUtil.arrayToString(keys.toArray()));
+    System.out.println(FormatUtil.arrayToString(vals.toArray()));
+    System.out.println(FormatUtil.arrayToString(types.toArray()));
+    System.out.println(obj.toString(2));
+    assertEquals(expected, count);
+  }
+}
---
+++ Util/src/test/java/org/gusdb/fgputil/JsonTypeTest.java
@@ -0,0 +1,88 @@
+package org.gusdb.fgputil;
+
+import static org.junit.Assert.assertEquals;
+
+import org.gusdb.fgputil.JsonType.ValueType;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class JsonTypeTest {
+
+  @Test
+  public void nullTest() throws Exception {
+    Object value = null;
+    JsonType json = new JsonType(value);
+    assertEquals(ValueType.NULL, json.getType());
+    assertEquals(JSONObject.NULL, json.get());
+    value = JSONObject.NULL;
+    json = new JsonType(value);
+    assertEquals(ValueType.NULL, json.getType());
+    assertEquals(JSONObject.NULL, json.get());
+  }
+
+  @Test
+  public void booleanTest() throws Exception {
+    String value = "true";
+    JsonType json = new JsonType(value);
+    assertEquals(ValueType.BOOLEAN, json.getType());
+    assertEquals(true, json.getBoolean());
+    value = "false";
+    json = new JsonType(value);
+    assertEquals(ValueType.BOOLEAN, json.getType());
+    assertEquals(false, json.getBoolean());
+  }
+
+  @Test
+  public void doubleTest() throws Exception {
+    String value = "123.123";
+    JsonType json = new JsonType(value);
+    assertEquals(ValueType.NUMBER, json.getType());
+    assertEquals(Double.valueOf(123.123), json.getDouble());
+  }
+
+  @Test
+  public void stringTest() throws Exception {
+    String value = "\"123.123\"";
+    JsonType json = new JsonType(value);
+    assertEquals(ValueType.STRING, json.getType());
+    assertEquals("123.123", json.getString());
+  }
+
+  @Test
+  public void arrayTest() throws Exception {
+    String value = "[ \"123.123\", true, null ]";
+    JsonType json = new JsonType(value);
+    assertEquals(ValueType.ARRAY, json.getType());
+    assertEquals(3, json.getJSONArray().length());
+  }
+
+  @Test
+  public void objectTest() throws Exception {
+    String value = "{ \"item1\": \"123.123\", \"item2\": true, \"item3\": null }";
+    JsonType json = new JsonType(value);
+    assertEquals(ValueType.OBJECT, json.getType());
+    assertEquals(3, json.getJSONObject().length());
+  }
+
+  // XXX: FYI this code SHOULD fail in many places, but org.json is NOT a strict parser and
+  //   accepts raw strings, both in objects and arrays
+  @Test
+  public void failureTest() throws Exception {
+    String value = "abc";
+    JsonType json = new JsonType(value);
+    assertEquals(ValueType.STRING, json.getType());
+    value = "[ abc, def ]";
+    json = new JsonType(value);
+    assertEquals(ValueType.ARRAY, json.getType());
+    assertEquals(2, json.getJSONArray().length());
+    value = "[ abc def ]";
+    json = new JsonType(value);
+    assertEquals(ValueType.ARRAY, json.getType());
+    assertEquals(1, json.getJSONArray().length());
+    JSONArray array = new JSONArray(value);
+    System.out.println(array);
+    JSONObject obj = new JSONObject("{ 1.2: b }");
+    System.out.println(obj.getString("1.2"));
+  }
+}
