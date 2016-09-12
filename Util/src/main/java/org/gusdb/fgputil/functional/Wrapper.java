d1fd84648e9ca0a3ef3623cbe7d0fb15fc2d967b
Ryan Doherty <tech@conical.org>
2016-09-12 03:04:12 +0000

    Create Wrapper class for use in 'fooling' the compiler to allow non-final variables in closures
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/FgpUtil/trunk@19084 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Util/src/main/java/org/gusdb/fgputil/functional/Wrapper.java
@@ -0,0 +1,45 @@
+package org.gusdb.fgputil.functional;
+
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * Wrapper around a reference to a typed Object.  Sometimes while writing anonymous functions we must make
+ * variables final that we might not otherwise in order to close over them.  In some cases making the object
+ * final is not feasible since not only the properties of the object may change, but the object itself may
+ * change value.  It may also be desirable to close over a primitive or immutable object (say, a String) which
+ * may change value.  This class is a way around these limitations since you can create a final instance of
+ * Wrapper and place your mutable object inside, changing its value at will either inside or outside your
+ * closing class.  This class is threadsafe but that does not protect mutations on the enclosed object.
+ * 
+ * @author rdoherty
+ *
+ * @param <T> the type of the object wrapped
+ */
+public class Wrapper<T> {
+
+  private T _obj;
+  private ReadWriteLock _lock = new ReentrantReadWriteLock();
+
+  public Wrapper() { }
+
+  public void set(T obj) {
+    try {
+      _lock.writeLock().lock();
+      _obj = obj;
+    }
+    finally {
+      _lock.writeLock().unlock();
+    }
+  }
+
+  public T get() {
+    try {
+      _lock.readLock().lock();
+      return _obj;
+    }
+    finally {
+      _lock.readLock().unlock();
+    }
+  }
+}
