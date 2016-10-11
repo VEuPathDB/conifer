5aa2d8978d85541ee952adf66cf75f29cdbedad8
Ryan Doherty <tech@conical.org>
2016-10-11 20:45:09 +0000

    Add second test for /var/tmp
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/FgpUtil/trunk@19273 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Util/src/test/java/org/gusdb/fgputil/IoUtilTest.java
+++ Util/src/test/java/org/gusdb/fgputil/IoUtilTest.java
@@ -1,5 +1,7 @@
 package org.gusdb.fgputil;
 
+import java.nio.file.Paths;
+
 import org.junit.Test;
 
 public class IoUtilTest {
@@ -7,5 +9,6 @@ public class IoUtilTest {
   @Test
   public void testDirectoryCreation() throws Exception {
     IoUtil.createOpenPermsTempDir(IoUtilTest.class.getName());
+    IoUtil.createOpenPermsTempDir(Paths.get("/var/tmp"), IoUtilTest.class.getName());
   }
 }
