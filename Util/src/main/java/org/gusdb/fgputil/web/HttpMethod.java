678bfb0a510d1fd6fd712561168d163cb441b8fa
Ryan Doherty <tech@conical.org>
2017-07-21 20:18:26 +0000

    Allow more comprehensive list of HTTP methods and don't throw if incoming value is unrecognized
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/FgpUtil/trunk@20436 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Util/src/main/java/org/gusdb/fgputil/web/HttpMethod.java
+++ Util/src/main/java/org/gusdb/fgputil/web/HttpMethod.java
@@ -1,19 +1,27 @@
 package org.gusdb.fgputil.web;
 
+import org.apache.log4j.Logger;
+
 public enum HttpMethod {
+  HEAD,
   POST,
   PUT,
   GET,
   PATCH,
-  DELETE;
+  DELETE,
+  CONNECT,
+  OPTIONS,
+  TRACE,
+  UNRECOGNIZED;
+
+  private static final Logger LOG = Logger.getLogger(HttpMethod.class);
 
   public static HttpMethod getValueOf(String method) {
     for (HttpMethod value : values()) {
       if (value.name().equalsIgnoreCase(method)) return value;
     }
-    throw new IllegalArgumentException("Method string '" + method +
-        "' does not represent a supported HTTP method.");
+    LOG.warn("HTTP Method submitted ('" + method + "') which does not represent a recognized HTTP method.");
+    return UNRECOGNIZED;
   }
 
-  
 }
