26d2206e4541e4b320367699ab25ec9597b58871
Bob Belnap <robert.belnap@uga.edu>
2017-10-30 16:08:22 +0000

    fix unquoted string
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/FgpUtil/trunk@21120 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Util/lib/perl/CommandHelper.pm
+++ Util/lib/perl/CommandHelper.pm
@@ -77,7 +77,7 @@ sub getSystemProps {
   #set the log4j configuration
   $sysProps .= " -Dlog4j.configuration=\"file://$GUS_HOME/config/log4j.properties\"";
   # use system cacerts if available
-  if (-e /etc/pki/tls/certs/cacerts ) {
+  if (-e "/etc/pki/tls/certs/cacerts" ) {
     $sysProps .= " -Djavax.net.ssl.trustStore=/etc/pki/tls/certs/cacerts";
   }
 
