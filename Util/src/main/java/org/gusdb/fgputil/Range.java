8d4ca5aa6f86cc09d4476b67953635c79626174e
Ryan Doherty <tech@conical.org>
2019-01-26 12:55:21 +0000

    Rename methods
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/FgpUtil/trunk@23806 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Util/src/main/java/org/gusdb/fgputil/Range.java
+++ Util/src/main/java/org/gusdb/fgputil/Range.java
@@ -4,17 +4,21 @@ import org.gusdb.fgputil.Tuples.TwoTuple;
 
 public class Range<T extends Comparable<T>> extends TwoTuple<T,T> {
 
-  public Range(T min, T max) {
-    super(min, max);
-    if (min != null && max != null && min.compareTo(max) > 0) {
-      throw new IllegalArgumentException("Min value cannot be greater than Max value.");
+  public static <T> T empty() { return null; }
+
+  public Range(T begin, T end) {
+    super(begin, end);
+    if (hasBegin() && hasEnd() && begin.compareTo(end) > 0) {
+      throw new IllegalArgumentException("Range's begin value cannot be greater than its end value.");
     }
   }
 
-  public boolean hasMin() { return getMin() != null; }
-  public boolean hasMax() { return getMax() != null; }
-
-  public T getMin() { return getFirst(); }
-  public T getMax() { return getSecond(); }
+  public boolean hasBegin() { return getBegin() != empty(); }
+  public void setBegin(T begin) { set(begin, getEnd()); }
+  public T getBegin() { return getFirst(); }
+  
+  public boolean hasEnd() { return getEnd() != empty(); }
+  public void setEnd(T end) { set(getBegin(), end); }
+  public T getEnd() { return getSecond(); }
 
 }
