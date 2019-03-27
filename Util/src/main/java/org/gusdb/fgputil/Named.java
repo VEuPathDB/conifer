69f71e1af9bdfb441b38ce05dea6287e84b87b8b
Elizabeth Harper <foxcapade@gmail.com>
2019-03-27 13:02:13 +0000

    Method reference & lambda cleanup of Named
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/FgpUtil/branches/strategy-loading@24444 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Util/src/main/java/org/gusdb/fgputil/Named.java
+++ Util/src/main/java/org/gusdb/fgputil/Named.java
@@ -1,7 +1,6 @@
 package org.gusdb.fgputil;
 
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.function.Function;
@@ -13,9 +12,9 @@ public class Named {
 
   private Named() {}
 
-  public static interface NamedObject {
-    public String getName();
-    public default String getFullName() {
+  public interface NamedObject {
+    String getName();
+    default String getFullName() {
       return getName();
     }
   }
@@ -23,42 +22,30 @@ public class Named {
   /**
    * Utility function that converts a named object to its name.  Handy for map operations, etc.
    */
-  public static final Function<NamedObject,String> TO_NAME = new Function<NamedObject,String>() {
-    @Override
-    public String apply(NamedObject namedObj) {
-      return namedObj.getName();
-    }
-  };
+  public static final Function<NamedObject,String> TO_NAME = NamedObject::getName;
 
   public static final <T extends NamedObject> Function<T, Entry<String, T>> getToMapEntry(
       @SuppressWarnings("unused") Class<T> namedClass) { // used by compiler to determine type of T
-    return new Function<T, Entry<String, T>>() {
-      @Override
-      public Entry<String, T> apply(T obj) {
-        return new TwoTuple<String, T>(obj.getName(), obj);
-      }
-    };
+    return obj -> new TwoTuple<>(obj.getName(), obj);
   }
 
   public static <T extends NamedObject> void sortByName(List<T> list, final SortDirection direction, final boolean ignoreCase) {
-    Collections.sort(list, new Comparator<T>() {
-      @Override public int compare(T obj1, T obj2) {
-        switch(direction) {
-          case DESC:
-            return ignoreCase ?
-                obj2.getName().compareToIgnoreCase(obj1.getName()) :
-                obj2.getName().compareTo(obj1.getName());
-          case ASC:
-          default:
-            return ignoreCase ?
-                obj1.getName().compareToIgnoreCase(obj2.getName()) :
-                obj1.getName().compareTo(obj2.getName());
-        }
-      }});
+    Collections.sort(list, (obj1, obj2) -> {
+      switch(direction) {
+        case DESC:
+          return ignoreCase ?
+              obj2.getName().compareToIgnoreCase(obj1.getName()) :
+              obj2.getName().compareTo(obj1.getName());
+        case ASC:
+        default:
+          return ignoreCase ?
+              obj1.getName().compareToIgnoreCase(obj2.getName()) :
+              obj1.getName().compareTo(obj2.getName());
+      }
+    });
   }
 
   public static <T extends NamedObject> Predicate<T> nameMatches(String name) {
     return named -> named.getName().equals(name);
   }
-
 }
