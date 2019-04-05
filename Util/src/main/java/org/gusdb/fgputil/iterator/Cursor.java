723530d67a6c2618758985f867953d00f7f2e89f
Ryan Doherty <tech@conical.org>
2019-04-05 04:59:48 +0000

    format comment
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/FgpUtil/branches/strategy-loading@24556 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Util/src/main/java/org/gusdb/fgputil/iterator/Cursor.java
+++ Util/src/main/java/org/gusdb/fgputil/iterator/Cursor.java
@@ -1,11 +1,14 @@
 package org.gusdb.fgputil.iterator;
 
 /**
- * The Iterator interface has two read methods: hasNext() and next().  Typically hasNext() is called first and if true is returned, next() can be called to fetch the next item (and move the iterator).
- * However, it is sometimes more convenient to implement a JDBC ResultSet-style interface where an attempt is
- * made to fetch the next object (moving the iterator up front), and then the current object can be accessed
- * over and over.  This interface codifies the second case.  The IteratorUtil class has methods to translate
- * from one interface to the other and back.
+ * The Iterator interface has two read methods: hasNext() and next().  Typically
+ * hasNext() is called first and if true is returned, next() can be called to
+ * fetch the next item (and move the iterator).  However, it is sometimes more
+ * convenient to implement a JDBC ResultSet-style interface where an attempt is
+ * made to fetch the next object (moving the iterator up front), and then the
+ * current object can be accessed over and over.  This interface codifies the
+ * second case.  The IteratorUtil class has methods to translate from one
+ * interface to the other and back.
  * 
  * @author rdoherty
  *
