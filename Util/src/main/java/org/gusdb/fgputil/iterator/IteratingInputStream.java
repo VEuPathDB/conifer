aac135f667a7f5b852399deb2da77f1f926c7852
Ryan Doherty <tech@conical.org>
2019-04-17 04:41:15 +0000

    remove unneeded override
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/FgpUtil/trunk@24638 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Util/src/main/java/org/gusdb/fgputil/iterator/IteratingInputStream.java
+++ Util/src/main/java/org/gusdb/fgputil/iterator/IteratingInputStream.java
@@ -94,38 +94,4 @@ public class IteratingInputStream extends InputStream {
     }
     return Byte.toUnsignedInt(_recordBuffer[_recordBufferIndex++]);
   }
-
-  /*
-  @Override
-  public int read(byte b[], int off, int len) throws IOException {
-    if (b == null) {
-        throw new NullPointerException();
-    } else if (off < 0 || len < 0 || len > b.length - off) {
-        throw new IndexOutOfBoundsException();
-    } else if (len == 0) {
-        return 0;
-    }
-
-    int c = read();
-    if (c == -1) {
-        return -1;
-    }
-    b[off] = (byte)c;
-
-    int i = 1;
-    try {
-        for (; i < len ; i++) {
-            c = read();
-            if (c == -1) {
-                break;
-            }
-            b[off + i] = (byte)c;
-        }
-    }
-    catch (IOException ee) {
-      LOG.error("Problem reading", ee);
-    }
-    return i;
-  }
-  */
 }
