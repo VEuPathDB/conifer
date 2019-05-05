eaaa5c0b4513c9cc128cf98b742397541b600d9b
Ryan Doherty <tech@conical.org>
2019-05-05 11:33:10 +0000

    remove unused import
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/FgpUtil/branches/strategy-loading@24772 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Util/src/main/java/org/gusdb/fgputil/FormatUtil.java
+++ Util/src/main/java/org/gusdb/fgputil/FormatUtil.java
@@ -11,15 +11,14 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.Date;
 import java.util.Arrays;
-import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.function.Function;
-import java.util.stream.Stream;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.gusdb.fgputil.functional.Functions;
 
