a2293d221938a1a41b814fc812ea8728d0ea3612
Ryan Doherty <tech@conical.org>
2019-09-12 19:02:59 +0000

    Add runtime utils
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/FgpUtil/trunk@25590 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Util/src/main/java/org/gusdb/fgputil/runtime/RuntimeUtil.java
@@ -0,0 +1,22 @@
+package org.gusdb.fgputil.runtime;
+
+import java.lang.reflect.Field;
+
+public class RuntimeUtil {
+
+  public static synchronized long getPid(Process p) {
+    long pid = -1;
+    try {
+      if (p.getClass().getName().equals("java.lang.UNIXProcess")) {
+        Field f = p.getClass().getDeclaredField("pid");
+        f.setAccessible(true);
+        pid = f.getLong(p);
+        f.setAccessible(false);
+      }
+    }
+    catch (Exception e) {
+      pid = -1;
+    }
+    return pid;
+  }
+}
