ad415676788004e53c4e1507d9b8a2b7d9206ac5
Ryan Doherty <tech@conical.org>
2019-09-12 19:35:26 +0000

    Add logging in error cases
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/FgpUtil/trunk@25591 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Util/src/main/java/org/gusdb/fgputil/runtime/RuntimeUtil.java
+++ Util/src/main/java/org/gusdb/fgputil/runtime/RuntimeUtil.java
@@ -2,19 +2,27 @@ package org.gusdb.fgputil.runtime;
 
 import java.lang.reflect.Field;
 
+import org.apache.log4j.Logger;
+
 public class RuntimeUtil {
 
+  private static final Logger LOG = Logger.getLogger(RuntimeUtil.class);
+
   public static synchronized long getPid(Process p) {
-    long pid = -1;
+    int pid = -1;
     try {
       if (p.getClass().getName().equals("java.lang.UNIXProcess")) {
         Field f = p.getClass().getDeclaredField("pid");
         f.setAccessible(true);
-        pid = f.getLong(p);
+        pid = f.getInt(p);
         f.setAccessible(false);
+      }
+      else {
+        LOG.warn("Process PID requested from non-unix process of type: " + p.getClass().getName());
       }
     }
     catch (Exception e) {
+      LOG.warn("Unable to look up UNIX process pid", e);
       pid = -1;
     }
     return pid;
